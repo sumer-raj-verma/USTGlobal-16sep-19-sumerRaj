c19cbb18ec03b2ce8ed8c09f21e451a7911d3b20
sumer-raj-verma <56624114+sumer-raj-verma@users.noreply.github.com>
2019-11-28 15:57:35 +0530

    Add files via upload

---
+++ Assessement_Module_Jdbc/mobileapp/src/com/ustglobal/mobileapp/dto/Person.java
@@ -0,0 +1,26 @@
+package com.ustglobal.mobileapp.dto;
+
+public class Person {
+	private String name;
+	private long pnumber;
+	private String groups;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPnumber() {
+		return pnumber;
+	}
+	public void setPnumber(long pnumber) {
+		this.pnumber = pnumber;
+	}
+	public String getGroups() {
+		return groups;
+	}
+	public void setGroups(String groups) {
+		this.groups = groups;
+	}
+}
